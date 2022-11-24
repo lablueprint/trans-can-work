b6b50a238c831aff7b3efa888a6c5f2dcfa3f4bf
angelahao6 <115599906+angelahao6@users.noreply.github.com>
2022-11-23 21:35:14 -0800

    added routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  Route, Routes,
+} from 'react-router-dom';
+import Header from './Components/Navigation/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HIII <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <p>hello</p>
+      <Routes>
+        <Route path="/" />
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Navigation/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+import React from 'react';
+
+export default function Header() {
+  return (
+  /* <nav className="header">
+      <div className="header-container"><a href="/html/">HTML</a></div>
+    </nav>
+    */
+    <p>hello!</p>
+  );
+}
