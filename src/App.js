7e0d8b2f6d372013b507c0a7107fad48811a09f9
angelahao6 <115599906+angelahao6@users.noreply.github.com>
2023-01-11 21:26:53 -0800

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from './Pages';
 import './App.css';
 import Footer from './Components/Footer/Footer';
+import Header from './Components/Navigation/Header'; 
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Header /> 
       <Footer />
     </div>
   );
