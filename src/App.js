8bb0b1aef86c628a9af3d95880d32c0276ef3ade
Alan Nguyen <alannguyen@s-128-97-104-212.resnet.ucla.edu>
2023-01-19 00:03:55 -0800

    YAY

--- src/App.js
+++ src/App.js
@@ -1,31 +1,116 @@
-// import logo from './logo.svg';
+import React from 'react';
+import {
+  Route, Routes,
+} from 'react-router-dom';
+import {
+  Home
+} from './Pages';
 import './App.css';
-import ProfileOutline from './Components/profileOutline'
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
-        <div>
-          <ProfileOutline 
-          name = {"kaylee"}
-          title = {"slayer"}
-          pronouns = {"she/her"}
-          email = {"kaeleytran@gmail.com"}
-          phone = {"714-420-6969"}
-          city = {"Orange County"}
-          state = {"California"}
-          ethnicity = {"vietnamese"}
-          age = {"69"}
-          genderIdentity = {"baddie"}
-          sexuality = {"your mom"}
-          veteran = {"duh"}
-          disability = {"many"}
-          housingSituation = {"stable"}
-          employmentStatus= {"space place"}
-          priorConvictions = {"hundreds"}
-          >
-          </ProfileOutline> 
-        </div>
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
 export default App;
+
+
+// // import logo from './logo.svg';
+// import './App.css';
+// import ProfileOutline from './Components/profileOutline'
+
+// // const demographic_info = [{
+// //   name: 'kaylee',
+// //   title: 'slayer',
+// //   pronouns: 'she/her',
+// //   email: 'kaeleytran@gmail.com',
+// //   phone: '714-420-6969',
+// //   city: 'Orange County',
+// //   state: 'California',
+// //   ethnicity: 'vietnamese',
+// //   age: 69,
+// //   genderIdentity: 'baddie',
+// //   sexuality: 'your mom',
+// //   veteran: 'duh',
+// //   disability: 'many',
+// //   housingSituation: 'stable',
+// //   employmentStatus: 'space place',
+// //   priorConvictions: 'hundreds'
+// // }]
+
+// function App() {
+//   return (
+//         <div>
+//         <ProfileOutline>
+//           demographic_info.
+//         </ProfileOutline>
+//           {/* <ProfileOutline 
+//           name = {"kaylee"}
+//           title = {"slayer"}
+//           pronouns = {"she/her"}
+//           email = {"kaeleytran@gmail.com"}
+//           phone = {"714-420-6969"}
+//           city = {"Orange County"}
+//           state = {"California"}
+//           ethnicity = {"vietnamese"}
+//           age = {"69"}
+//           genderIdentity = {"baddie"}
+//           sexuality = {"your mom"}
+//           veteran = {"duh"}
+//           disability = {"many"}
+//           housingSituation = {"stable"}
+//           employmentStatus= {"space place"}
+//           priorConvictions = {"hundreds"}
+//           >
+//           </ProfileOutline>  */}
+//         </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// // // import logo from './logo.svg';
+// // import './App.css';
+// // import ProfileOutline from './Components/profileOutline';
+
+// // function App() {
+
+// //   return (
+
+// // <div>
+// //   <ProfileOutline 
+// //           name = {"kaylee"}
+// //           title = {"slayer"}
+// //           pronouns = {"she/her"}
+// //           email = {"kaeleytran@gmail.com"}
+// //           phone = {"714-420-6969"}
+// //           city = {"Orange County"}
+// //           state = {"California"}
+// //           ethnicity = {"vietnamese"}
+// //           age = {69}
+// //           genderIdentity = {"baddie"}
+// //           sexuality = {"your mom"}
+// //           veteran = {"duh"}
+// //           disability = {"many"}
+// //           housingSituation = {"stable"}
+// //           employmentStatus= {"space place"}
+// //           priorConvictions = {"hundreds"}
+// //           >
+// //    </ProfileOutline>
+// //   </div>
+      
+// //   );
+// // }
+
+// // export default App;
--- src/Components/demographicInfo.js
+++
@@ -1,3 +0,0 @@
-// apparently this file is gonna look like what profileOutline.js has right now
-// and then we would basically just pass the demographic info object or whatever into profile outline 
-// and then the profile outline would just export that if i am not mistaken
