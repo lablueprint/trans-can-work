a7c5a9ddbd7ad2b163748b279c8d7cb34889f7aa
angelahao6 <115599906+angelahao6@users.noreply.github.com>
2023-01-25 22:27:08 -0800

    Created milestone button and popup

---
+++ src/Components/Milestones/MilestoneButton.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+function MilestoneButton({title, onClick}) {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    return (
+        <div>
+            <Button onClick={handleOpen}>{title}</Button>
+            <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            >
+            <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h3" component="h6">
+                Milestone Details
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 60 }}>
+                Here is a milestone description!
+                </Typography>
+            </Box>
+            </Modal>
+        </div>
+        
+    );
+    
+}
+
+MilestoneButton.propTypes = {
+    title: PropTypes.string,
+}
+
+export default MilestoneButton  
