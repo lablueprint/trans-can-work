9a41a107c93fa20c8cf7b78852f73c857de23cfe
kevantony <kevin.m.antony@gmail.com>
2023-02-01 22:24:16 -0800

    update state setting to avoid multiple rerenders

--- src/Pages/NavigatorDashboard.js
+++ src/Pages/NavigatorDashboard.js
@@ -4,30 +4,51 @@ import { db } from "../Components/firebase";
 import "./NavigatorDashboard.css"
 
 
+const getSeeker = async (setJobseeker) => {
+    const Ref = collection(db, "jobseekers");
+
+const q = query(Ref, where("name", "==", "Joe Bruin"));
+const querySnapshot = await getDocs(q);
+querySnapshot.forEach((doc) => {
+    const data = doc.data();
+    console.log(data)
+    setJobseeker({
+        name: data.name,
+        pronouns: data.pronouns,
+        phone: data.phone,
+        email: data.email,
+        ethnicity: data.ethnicity,
+        degree: data.degree,
+        degreeType: data.degreeType,
+        certificate: data.certificate,
+        certificateType: data.certificateType,
+    })
+});
+}
 
 function NavigatorDashboard() {
-    const temp = async () => {
-        const Ref = collection(db, "jobseekers");
 
-    const q = query(Ref, where("name", "==", "Joe Bruin"));
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
-    const data = doc.data();
-    setName(data.name)
-    setPronouns(data.pronouns)
-    setPhone(data.phone)
-    setEmail(data.email)
-    setEthnicity(data.ethnicity)
-    });
-    }
-    temp();
 
-    const [name, setName] = useState("Name")
-    const [pronouns, setPronouns] = useState("Pronouns")
-    const [phone, setPhone] = useState("Phone")
-    const [email, setEmail] = useState("Email")
-    const [ethnicity, setEthnicity] = useState("Ethnicity")
+    const updateJobseeker = () => {
+        console.log(1)
+    };
+    
+    const [jobseeker, setJobseeker] = useState({
+        name: "Name",
+        pronouns: "Pronouns",
+        phone: "Phone",
+        email: "Email",
+        ethnicity: "Ethnicity",
+        degree: "No",
+        degreeType: "None",
+        certificate: "No",
+        certificateType: "None",
+    })
+    const {name, pronouns, phone, email, ethnicity, degree, degreeType, certificate, certificateType} = jobseeker
+    useEffect(() => {
+        getSeeker(setJobseeker)
+    }, [setJobseeker])
+    
   return (
     <div>
         <div>
@@ -72,33 +93,33 @@ function NavigatorDashboard() {
                 <form>
                     <div className="inputWrapper">
                         <label for="degree">Degree?</label>
-                        <input id="degree" placeholder={name} type="text"></input>
+                        <select id="degree" placeholder={degree} type="text">
+                            <option value="yes">Yes</option>
+                            <option value="no">No</option>
+                        </select>
                     </div>
                     <div className="inputWrapper">
                         <label for="degreeType">Type of Degree</label>
-                        <input id="degreeType" placeholder={pronouns} type="text"></input>
+                        <input id="degreeType" placeholder={degreeType} type="text"></input>
                     </div>
                     <div className="inputWrapper">
                         <label for="cert">Certificate?</label>
-                        <select id="cert" placeholder="Select" type="text">
+                        <select id="cert" placeholder={certificate} type="text">
                             <option value="yes">Yes</option>
                             <option value="no">No</option>
                         </select>
                     </div>
                     <div className="inputWrapper">
                         <label for="certType">Type of Certificate</label>
-                        <input id="certType" placeholder={email} type="text"></input>
+                        <input id="certType" placeholder={certificateType} type="text"></input>
                     </div>
                     {/* <div className="inputWrapper">
                         <label for="location">City/State</label>
                         <input id="location" placeholder="Kevin" type="text"></input>
                     </div> */}
-                    <div className="inputWrapper">
-                        <label for="ethnicity">Ethnicity</label>
-                        <input id="ethnicity" placeholder={ethnicity} type="text"></input>
-                    </div>
                     {/* Submit button and on Click we update info */}
                 </form>
+                <button onClick={updateJobseeker}>Save Changes</button>
             </div>
         </div>
     </div>
