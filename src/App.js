6c6c0e3219764218499b4a0ff30bc08de303405d
angelahao6 <115599906+angelahao6@users.noreply.github.com>
2023-02-13 19:37:19 -0800

    added milestone button in app.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,18 @@ import {
 } from './Pages';
 import './App.css';
 import Footer from './Components/Footer/Footer';
+import MilestoneButton from './Components/Milestones/MilestoneButton';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <MilestoneButton title="Click Me!"></MilestoneButton>
       <Routes>
         <Route path="/" element={<Home />} />
-      </Routes>
+      </Routes> 
       <Footer />
     </div>
   );
