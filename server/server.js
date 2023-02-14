d127f7c428a2e7aa32265ced1371140ed48d3085
Ryan Yeo <ryandc.yeo@gmail.com>
2023-02-13 22:10:52 -0800

    server setup incomplete

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+app.get('/api', (req, res) => {
+    // res.json({ message: "Hello from server!" });
+    res.send('hello world from express');
+});
+
+app.listen(1234);
+// app.listen(PORT, () => {
+//     console.log(`Server listening on ${PORT}`);
+// });
