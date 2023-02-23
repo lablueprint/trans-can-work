016860086bf3b71d9a65dbb24a6c133db889461d
Kaylee Tran <kayleetran@Kaylees-Macbook-Pro.local>
2023-02-22 22:55:27 -0800

    page to route navigator dashboard

---
+++ src/Pages/NavigatorDashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavigatorDashboard from "../Components/navigatorDashboard";
+export default function NavigatorDashboard() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <NavigatorDashboard></NavigatorDashboard>
+          </div>
+        </header>
+      </div>
+    );
+  }
