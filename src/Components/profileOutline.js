012f5bce6bf628b63b5f8c0d2529fa9e0add1327
Solia Valentine <soliavalentine@gmail.com>
2023-03-01 21:48:25 -0800

    Profile outline lint fix

--- src/Components/profileOutline.js
+++ src/Components/profileOutline.js
@@ -1,15 +1,16 @@
-import React, {useState, useEffect} from 'react';
+import React, { useEffect } from 'react';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { PropTypes } from 'prop-types';
 import Avatar from 'react-avatar';
-import {fetchByNavigator, fetchAllJobseekers, fetchJobseeker, updateJobseeker} from '../Services/jobseeker-service';
-import {updateNavigator} from '../Services/navigator-service';
-import {updateAdmin} from '../Services/admin-service';
+import Button from '@mui/material/Button';
+import {
+  fetchByNavigator, fetchAllJobseekers,
+} from '../Services/jobseeker-service';
 
-const demographic_info = [{
+const demographicInfo = [{
   name: 'kaylee',
   title: 'slayer',
   pronouns: 'she/her',
@@ -25,12 +26,13 @@ const demographic_info = [{
   disability: 'many',
   housingSituation: 'a shit show',
   employmentStatus: 'space place',
-  priorConvictions: 'hundreds'
-}]
-
+  priorConvictions: 'hundreds',
+}];
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+  const {
+    children, value, index, ...other
+  } = props;
 
   return (
     <div
@@ -56,212 +58,206 @@ function a11yProps(index) {
   };
 }
 
-const initialValues = {
-  name: demographic_info[0].name,
-  pronouns: demographic_info[0].pronouns,
-}
+// const initialValues = {
+//   name: demographicInfo[0].name,
+//   pronouns: demographicInfo[0].pronouns,
+// };
 
 export default function ProfileOutline() {
   const [value, setValue] = React.useState(0);
-  const [disableButton, setDisableButton] = React.useState(true) 
-  const [values, setValues] = React.useState(initialValues);
-  const [clients, setClients] = React.useState([]);
+  const [disableButton, setDisableButton] = React.useState(true);
+  // const [values, setValues] = React.useState(initialValues);
+  // const [clients, setClients] = React.useState([]);
 
-  //so since we are NOT reusing this component for both personal profiles
-  //and looking at other account profiles, we would use userData
-  //and would have different initialValues, but not a backend call for it.
+  // so since we are NOT reusing this component for both personal profiles
+  // and looking at other account profiles, we would use userData
+  // and would have different initialValues, but not a backend call for it.
 
-  //another important point: the states, e.g. values and disable buttons being set to userData
-  // and false mean that if changes are made and not saved, the changes are reverted. (desired behavior)
+  // another important point: the states, e.g. values and disable buttons being set to userData
+  // and false mean that if changes are made and not saved, the changes are reverted.
 
-  //sample info from userData:
-  const userType = "navigator"; // or navigator or jobseeker
+  // sample info from userData:
+  const userType = 'navigator'; // or navigator or jobseeker
   const isApproved = false;
-  // const email = "nasser@bp.com";
-  const email = "solia@test.edu";
-  // const email = "tcw@slays.com"
 
   useEffect(() => {
     const logquery = async () => {
       const query = await fetchAllJobseekers();
       console.log(query);
-    }
+    };
     logquery();
   }, []);
 
-
-
   useEffect(() => {
     const processNav = async () => {
-      const clientList = await fetchByNavigator("solia@test.edu");
-      console.log("clientList:");
+      const clientList = await fetchByNavigator('solia@test.edu');
+      console.log('clientList:');
       console.log(clientList);
-      setClients(clientList);
-    }
+      // setClients(clientList);
+    };
 
     const processAdmin = async () => {
       const clientList = await fetchAllJobseekers();
       console.log(clientList);
-      setClients(clientList);
-    }
+      // setClients(clientList);
+    };
 
-    if (userType === "navigator"){
-      console.log("hi nav");
+    if (userType === 'navigator') {
       processNav();
-    }
-    else if (userType === "admin" && isApproved){
-      console.log("hi admin");
+    } else if (userType === 'admin' && isApproved) {
       processAdmin();
     }
-
-  }, []); 
+  }, []);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  //i genuinely have no clue how this works:
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-  };
+  // i genuinely have no clue how this works:
+  // const handleInputChange = (e) => {
+  //   const { name, value } = e.target;
+  // };
 
   const handleToggle = (e) => {
     e.preventDefault();
-    if (!disableButton){ //then we want to save changes.
-      if (userType === "navigator"){
-        //Since values is currently useless:
-        //but when we do have a json of new fields, then this would work: 
-        // console.log(values); 
+    if (!disableButton) { // then we want to save changes.
+      if (userType === 'navigator') {
+        // Since values is currently useless:
+        // but when we do have a json of new fields, then this would work:
+        // console.log(values);
         // updateNavigator(email, values);
-      }
-      else if (userType === "admin" && isApproved){
+      } else if (userType === 'admin' && isApproved) {
         // console.log(values);
         // updateAdmin(email, values);
-      }
-      else if (userType === "jobseeker"){        
+      } else if (userType === 'jobseeker') {
         // updateJobseeker(email, values);
       }
     }
 
-    setDisableButton(!disableButton)
+    setDisableButton(!disableButton);
   };
 
-  if (userType === "navigator" || (userType === "admin" && isApproved) ) {
+  if (userType === 'navigator' || (userType === 'admin' && isApproved)) {
     return (
       <div className="App">
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={value}
+          <Tabs
+            value={value}
             onChange={handleChange}
-            aria-label="basic tabs example">
+            aria-label="basic tabs example"
+          >
             <Tab label="My Profile" {...a11yProps(0)} />
-            <Tab label="My Clients" {...a11yProps(1)} /> 
+            <Tab label="My Clients" {...a11yProps(1)} />
           </Tabs>
         </Box>
-        <TabPanel value={value} index={0} >
-        <Avatar facebookId="100008343750912" size="150" />
+        <TabPanel value={value} index={0}>
+          <Avatar facebookId="100008343750912" size="150" />
           <header>
             MY PROFILE
           </header>
-          <label>
+          <label htmlFor="Name">
             Name:
             <input
-              defaultValue={demographic_info[0].name}
-              onChange={handleInputChange}
+              id="Name"
+              defaultValue={demographicInfo[0].name}
               disabled={disableButton}
             />
           </label>
-          <br></br>
-          <label>
+          <br />
+          <label htmlFor="Pronouns">
             Pronouns:
             <input
-              defaultValue={demographic_info[0].pronouns}
-              onChange={handleInputChange}
+              id="Pronouns"
+              defaultValue={demographicInfo[0].pronouns}
               disabled={disableButton}
             />
           </label>
-          <br></br>
-          <p>Email: {demographic_info[0].email}</p>
-          <label>
+          <br />
+          <p>
+            Email:
+            {demographicInfo[0].email}
+          </p>
+          <label htmlFor="Bio">
             Bio:
             <input
-              defaultValue={""}
-              onChange={handleInputChange}
+              id="Bio"
+              defaultValue=""
               disabled={disableButton}
             />
           </label>
-          <br></br>
-          <button onClick={() => setDisableButton(!disableButton)}>
-            <i
-              style={{ color: "blue" }}
-            ></i>
-          </button>
+          <br />
+          <Button onClick={() => setDisableButton(!disableButton)}>
+            <i />
+          </Button>
         </TabPanel>
         <TabPanel value={value} index={1}>
-          clients go here 
+          clients go here
         </TabPanel>
 
       </div>
     );
-  } 
-  else if (userType === "jobseeker") {
-    return(
-    <div className="App">
+  }
+  if (userType === 'jobseeker') {
+    return (
+      <div className="App">
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={value}
+          <Tabs
+            value={value}
             onChange={handleChange}
-            aria-label="basic tabs example">
-            <Tab label="My Profile" {...a11yProps(0)} /> 
+            aria-label="basic tabs example"
+          >
+            <Tab label="My Profile" {...a11yProps(0)} />
           </Tabs>
         </Box>
-        <TabPanel value={value} index={0} >
-        <Avatar facebookId="100008343750912" size="150" />
-          <br></br>
-          <label>
+        <TabPanel value={value} index={0}>
+          <Avatar facebookId="100008343750912" size="150" />
+          <br />
+          <label htmlFor="Name">
             Name:
             <input
-              defaultValue={demographic_info[0].name}
-              onChange={handleInputChange}
+              id="Name"
+              defaultValue={demographicInfo[0].name}
               disabled={disableButton}
             />
           </label>
-          <br></br>
-          <label>
+          <br />
+          <label htmlFor="Pronouns">
             Pronouns:
             <input
-              defaultValue={demographic_info[0].pronouns}
-              onChange={handleInputChange}
+              id="Pronouns"
+              defaultValue={demographicInfo[0].pronouns}
               disabled={disableButton}
             />
           </label>
-          <br></br>
-          <p>Email: {demographic_info[0].email}</p>
+          <br />
+          <p>
+            Email:
+            {demographicInfo[0].email}
+          </p>
           <p>My Navigator: You</p>
-          <label>
+          <label htmlFor="Bio">
             Bio:
             <input
-              defaultValue={""}
-              onChange={handleInputChange}
+              id="Bio"
+              defaultValue=""
               disabled={disableButton}
             />
           </label>
-          <br></br>
-          <button onClick={handleToggle}>
-            <i
-              style={{ color: "blue" }}
-            ></i>
-          </button>
+          <br />
+          <Button onClick={handleToggle}>
+            <i />
+          </Button>
         </TabPanel>
-  </div>
-    )
-  }
-  else {
-    //TODO: here would be the splash screen for "waiting for admin approval" 
-    return(
-      <div>
-        Waiting for admin approval...
       </div>
-    )
+    );
   }
+
+  // TODO: here would be the splash screen for "waiting for admin approval"
+  return (
+    <div>
+      Waiting for admin approval...
+    </div>
+  );
 }
 
 ProfileOutline.propTypes = {
@@ -281,4 +277,9 @@ ProfileOutline.propTypes = {
   housingSituation: PropTypes.string.isRequired,
   employmentStatus: PropTypes.string.isRequired,
   priorConvictions: PropTypes.string.isRequired,
-}
+};
+TabPanel.propTypes = {
+  value: PropTypes.number.isRequired,
+  index: PropTypes.number.isRequired,
+  children: PropTypes.string.isRequired,
+};
