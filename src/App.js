42b90a80a33590a17473d209b6f848dc9390d74f
Diya Baliga <diyabaliga@Diyas-MacBook-Air.local>
2023-03-06 22:26:51 -0800

    initial version without full styling

--- src/App.js
+++ src/App.js
@@ -13,11 +13,12 @@ import {
 import './App.css';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Navigation/Header';
+import Splash from './Components/Splash';
+import approvalIcon from './Assets/mobile_friendly_24px.png';
 
 function App() {
   return (
     <div className="App">
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -25,6 +26,16 @@ function App() {
         <Route path="/dashboard/navigator" element={<NavigatorDashboard />} />
         <Route path="/reset" element={<Reset />} />
         <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/splash"
+          element={(
+            <Splash
+              header="Awaiting Approval"
+              description="You have successfully signed up for an account. Please await approval from a TransCanWork Administator."
+              graphic={<img alt="" src={approvalIcon} />}
+            />
+          )}
+        />
       </Routes>
       <Header />
       <Footer />
