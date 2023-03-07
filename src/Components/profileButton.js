1f6d29598686e315988de5b05c94f89b925d38bd
Kaylee Tran <kayleetran@s-169-232-215-213.resnet.ucla.edu>
2023-03-06 22:18:08 -0800

    profile button semi styled with modal component (does not display user with modal yet)

--- src/Components/profileButton.js
+++ src/Components/profileButton.js
@@ -1,38 +1,63 @@
-import React from "react";
-import { Avatar } from "@material-ui/core";
-import firebase from '../firebase';
-import {doc, setDoc } from "firebase/firestore";
+import React, { useState, useEffect } from 'react';
+import { Modal, Avatar, Button } from '@material-ui/core';
+// import { doc } from 'firebase/firestore';
+// import firebase from '../firebase';
 import { fetchJobseeker } from '../Services/jobseeker-service';
-import { useState,useEffect } from "react";
-
+import './profileButton.css';
+// import ProfileOutline from './profileOutline';
 
 function ProfileButton() {
-    let testemail = "solia@gmail.com";
-    var db = firebase;
-    const docRef = doc(db, "jobseekers", testemail);
-    const [name, setName] = useState('');
-    const [field, setField] = useState('');
+  const testemail = 'solia@gmail.com';
+  // const db = firebase;
+  const [name, setName] = useState('');
+  const [field, setField] = useState('');
+  const [viewProfile, setViewProfile] = useState(false);
 
+  useEffect(() => {
+    const logquery = async () => {
+      const query = await fetchJobseeker(testemail);
+      setName(query.data().name);
+      setField(query.data()['field of work']);
+    };
 
-    useEffect(() => {
-        const logquery = async () => {
-          const query = await fetchJobseeker(testemail);
-          setName(query.data()["name"]);
-          setField (query.data()["field of work"]);
-          console.log(field);
-        }
-     
-        logquery()
-      }, []);
-console.log(name);
-    return (
-      <div>
-        <Avatar facebookId="100008343750912" size="150" />
-        {name} <br/>
-        <text>name</text>
-        <text>field</text>
+    logquery();
+  }, []);
+
+  const handleOpen = () => {
+    setViewProfile(true);
+  };
+  const handleClose = () => {
+    setViewProfile(false);
+  };
+
+  return (
+    <div className="outer-container">
+      <div className="container">
+        <Avatar
+          facebookId="100008343750912"
+          style={{
+            height: '68px',
+            width: '68px',
+            marginTop: '6px',
+            marginBottom: '6px',
+            marginLeft: '6px',
+          }}
+        />
       </div>
-    );
-  }
-  
-  export default ProfileButton;
+      <br />
+      <div className="container2">
+        <text>{name}</text>
+        <text>{field}</text>
+        <Button onClick={handleOpen}>View Profile</Button>
+        <Modal
+          open={viewProfile}
+          onClose={handleClose}
+        >
+          <Button onClick={handleClose}>close</Button>
+        </Modal>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileButton;
