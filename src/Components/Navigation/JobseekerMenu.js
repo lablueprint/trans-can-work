8419869c0bf6bc70d549a3094f24ce80bed2d28d
Dan Lee <danlee01@ucla.edu>
2023-03-09 15:10:53 -0800

    Add JobseekerNav and Jobseeker Menu

---
+++ src/Components/Navigation/JobseekerMenu.js
@@ -0,0 +1,54 @@
+/* eslint-disable */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Dialog, Slide, List, ListItem, ListItemButton,
+} from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const options = [
+  { name: 'Assessment', link: '/assessment' },
+  { name: 'Online Profiles', link: '/onlineprofiles' },
+  { name: 'Training Programs', link: '/assessment' },
+  { name: 'Co-Enroll', link: '/assessment' },
+  { name: 'Workshops', link: '/assessment' },
+  { name: 'Internships', link: '/assessment' },
+  { name: 'Job Fairs', link: '/assessment' },
+  { name: 'Job Boards', link: '/assessment' },
+  { name: 'Resources', link: '/assessment' },
+  { name: 'Hiring Info', link: '/assessment' },
+];
+
+const Transition = React.forwardRef((props, ref) => <Slide direction="right" ref={ref} {...props} />);
+
+function JobseekerMenu({ open, handleClose }) {
+  const links = options.map((x) => (
+    <ListItem disableGutters>
+      <ListItemButton>
+        <Link to={x.link}>
+          {x.name}
+        </Link>
+      </ListItemButton>
+    </ListItem>
+  ));
+  return (
+    <Dialog
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={handleClose}
+      aria-describedby="alert-dialog-slide-description"
+    >
+      <List>
+        {links}
+      </List>
+    </Dialog>
+  );
+}
+
+export default JobseekerMenu;
+
+JobseekerMenu.propTypes = {
+  open: PropTypes.bool.isRequired,
+};
--- src/Components/Navigation/JobseekerNav.js
+++ src/Components/Navigation/JobseekerNav.js
@@ -1,77 +1,76 @@
+/* eslint-disable */
 import React from 'react';
-import { Button, IconButton } from '@mui/material';
-import GridViewRoundedIcon from '@mui/icons-material/GridViewRounded';
+import {
+  IconButton, Dialog, Slide, List, ListItem, ListItemButton
+} from '@mui/material';
+import { GridViewRounded } from '@mui/icons-material';
+import JobseekerMenu from './JobseekerMenu';
 
 const BUTTON_STYLE = {
   padding: 0,
-  background: "#FFFBFE",
-  boxSizing: "200%",
-  borderRadius: "1em",
-  boxSizing: "border-box",
-  fontSize: 80,
-}
+  background: '#FFFBFE',
+  borderRadius: '1em',
+  boxSizing: 'border-box',
+  fontSize: '2em',
+};
 
 const BIGGER_STYLE = {
-  background: "#FFFBFE",
-  "&:hover": {
-    background: "#FFFBFE"
-  }
-}
+  background: '#FFFBFE',
+  '&:hover': {
+    background: '#FFFBFE',
+  },
+};
+
+
+
 
 function JobseekerNav() {
-    const [open, setOpen] = React.useState(false);
-    const anchorRef = React.useRef(null);
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
 
-    const handleToggle = () => {
-        setOpen((prevOpen) => !prevOpen);
-    };
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
 
-    const handleClose = (event) => {
-        if (
-            anchorRef.current &&
-            anchorRef.current.contains(event.target)
-        ) {
-        return;
-        }
+  const handleClose = (event) => {
+    if (
+      anchorRef.current
+      && anchorRef.current.contains(event.target)
+    ) {
+      return;
+    }
 
-        setOpen(false);
-    };
+    setOpen(false);
+  };
 
-    function handleListKeyDown(event) {
-        if (event.key === 'Tab') {
-        event.preventDefault();
-          setOpen(false);
-        } else if (event.key === 'Escape') {
-          setOpen(false);
-        }
-      }
-    
-      // return focus to the button when we transitioned from !open -> open
-      const prevOpen = React.useRef(open);
-      React.useEffect(() => {
-        if (prevOpen.current === true && open === false) {
-          anchorRef.current?.focus();
-        }
-    
-        prevOpen.current = open;
-      }, [open]); 
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current?.focus();
+    }
 
-      return (
-        <div>
-        <IconButton
-          ref={anchorRef}
-          id="composition-button"
-          aria-controls={open ? 'composition-menu' : undefined}
-          aria-expanded={open ? 'true' : undefined}
-          aria-haspopup="true"
-          onClick={handleToggle}
-          size="large"
-          sx={BIGGER_STYLE}
-        >
-          <GridViewRoundedIcon sx={BUTTON_STYLE}/>
-        </IconButton>
-        </div>
-      )
+    prevOpen.current = open;
+  }, [open]);
+
+
+  return (
+    <div>
+      <IconButton
+        ref={anchorRef}
+        id="composition-button"
+        aria-controls={open ? 'composition-menu' : undefined}
+        aria-expanded={open ? 'true' : undefined}
+        aria-haspopup="true"
+        onClick={handleToggle}
+        size="large"
+        sx={BIGGER_STYLE}
+      >
+        <GridViewRounded sx={BUTTON_STYLE} />
+      </IconButton>
+      <JobseekerMenu open={open} handleClose={handleClose} />
+    </div>
+  );
 }
 
 export default JobseekerNav;
@@ -79,6 +78,7 @@ export default JobseekerNav;
 /* NOTES
     Nav for Jobseeker view
         - button thing that pops up a list of links to stuff
+        - https://mui.com/material-ui/react-dialog/#transitions
 
     We use emails to identify users (jobseeker/nav/admin)
         use static values for now
@@ -86,8 +86,6 @@ export default JobseekerNav;
 
     NEXT STEPS:
       1. add little bar thing at bottom of icon button
-      2. add popup
       3. do links + styling
-      
 
 */
