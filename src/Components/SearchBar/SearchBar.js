c93193b889ca27de5cb6315747245d8315ce0597
Alan Nguyen <alannguyen@Alans-MacBook-Pro.local>
2023-04-03 22:34:56 -0700

    es lint fixing

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,52 +1,48 @@
-import { ForkRightOutlined } from '@mui/icons-material';
-import React, {useState, useEffect} from 'react';
-import {updateJobseeker, deleteJobseeker, 
-  fetchAllJobseekers, fetchJobseeker, createJobseeker} from "../../Services/jobseeker-service.js";
+import React, { useState, useEffect } from 'react';
+import {
+  fetchAllJobseekers,
+} from '../../Services/jobseeker-service';
 
-function SearchBar(props)
-{
+function SearchBar() {
   const [value, setValue] = useState('');
-  const [result, setResult] = useState([])
-  let names = []
+  const [result, setResult] = useState([]);
+  const names = [];
 
   useEffect(() => {
     fetchAllJobseekers()
-            .then(docs => {docs.forEach(doc => {
-              names.push(doc.data()['name']);
-                   })
-                   names.sort();
-                  if (value.length > 0) {
-                    setResult([]);
-                    let searchQuery = value.toLowerCase();
-                    for(let step = 0; step < names.length; step++) {
-                      let nam = names[step].toLowerCase();
-                      if(nam.slice(0, searchQuery.length).indexOf(searchQuery) !== -1) {
-                        setResult(prevResult => {
-                          return [...prevResult, names[step]]
-                        })
-                      }
-                    }
-                  }
-                  else {
-                    setResult([]);
-                  }}
-                   
-                   );
-  }, [value])
+      .then((docs) => {
+        docs.forEach((doc) => {
+          names.push(doc.data().name);
+        });
+        names.sort();
+        if (value.length > 0) {
+          setResult([]);
+          const searchQuery = value.toLowerCase();
+          for (let step = 0; step < names.length; step += 1) {
+            const nam = names[step].toLowerCase();
+            if (nam.slice(0, searchQuery.length).indexOf(searchQuery) !== -1) {
+              setResult((prevResult) => [...prevResult, names[step]]);
+            }
+          }
+        } else {
+          setResult([]);
+        }
+      });
+  }, [value]);
 
-
-  return(
+  return (
     <div>
-      <header className="App-header"> 
-      <p className="titleText"> Search Bar </p>
-        <input type="text"
-        className="searchBar"
-        onChange={(event) => setValue(event.target.value)}
-        value={value}
+      <header className="App-header">
+        <p className="titleText"> Search Bar </p>
+        <input
+          type="text"
+          className="searchBar"
+          onChange={(event) => setValue(event.target.value)}
+          value={value}
         />
         <div className="searchBack">
           {result.map((results, index) => (
-            <a href="#" key={index}>
+            <a href="#/" key={index.id}>
               <div className="searchEntry">
                 {results}
               </div>
