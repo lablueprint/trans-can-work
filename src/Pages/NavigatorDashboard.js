fb6f21f6c1d9970ddef61161beb12e5cdcd14c7e
Kaylee Tran <kayleetran@g.ucla.edu>
2023-04-05 21:50:03 -0700

    bug fixing for profile trying to fix divergent branches

--- src/Pages/NavigatorDashboard.js
+++ src/Pages/NavigatorDashboard.js
@@ -160,17 +160,3 @@ function NavigatorDashboard() {
   );
 }
 export default NavigatorDashboard;
-import React from "react";
-import NavigatorDash from "../Components/navigatorDashboard";
-
-export default function NavigatorDashboard() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div>
-            <NavigatorDash></NavigatorDash>
-          </div>
-        </header>
-      </div>
-    );
-  }
