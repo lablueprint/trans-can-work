6138b203d58ecd7c2176effdf0b44123b0b36a85
Alan Nguyen <alannguyen@Alans-MacBook-Pro.local>
2023-04-10 21:17:52 -0700

    Start of landing page with tabs

---
+++ src/Pages/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandingPage from '../Components/landingPage';
+
+export default function Landing() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LandingPage />
+      </header>
+    </div>
+  );
+}
