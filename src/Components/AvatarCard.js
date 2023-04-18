efcf4be5f121ce0349d22bfc790fa3ffcc824510
Aaron Shi <aaronshi@Aarons-MacBook-Pro.local>
2023-04-17 20:57:24 -0700

    demo day created a spreadsheet

--- src/Components/AvatarCard.js
+++ src/Components/AvatarCard.js
@@ -1,7 +1,7 @@
 import {
   Paper, Avatar,
 } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
@@ -10,20 +10,42 @@ import {
   doc, setDoc, deleteDoc,
 } from 'firebase/firestore';
 import { PropTypes } from 'prop-types';
-import { db } from './firebase';
+import jwt_decode from 'jwt-decode';
 import './AvatarCard.css';
+
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+// import {
+//   useAuthState,
+// } from 'react-firebase-hooks/auth';
+import {
+  db, auth, // logInWithEmailAndPassword, logout,
+} from './firebase';
 
 const options = [
   'Unarchive',
   'Delete',
+  'Download',
 ];
 
 function AvatarCard({ user, archivedUsers, setArchivedUsers }) {
+  const CLIENTID = '309643493967-7i1u1sdkg51j85ai9vehms86hpf7d1op.apps.googleusercontent.com';
+  const SCOPES = 'https://www.googleapis.com/auth/drive';
+
   const [anchorEl, setAnchorEl] = useState(null);
+  const [authuser, setUser] = useState({});
+  const [tokenClient, setTokenClient] = useState('');
+
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
+
+  function handleCallbackResponse(response) {
+    console.log(`Encoded JWT ID token: ${response.credential}`);
+    const userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+  }
 
   const updateArchived = async (id) => {
     const Ref = doc(db, 'jobseekers', id);
@@ -32,6 +54,54 @@ function AvatarCard({ user, archivedUsers, setArchivedUsers }) {
 
   const deleteDocument = async (id) => {
     await deleteDoc(doc(db, 'jobseekers', id));
+  };
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: CLIENTID,
+      callback: handleCallbackResponse,
+    });
+
+    setTokenClient(google.accounts.oauth2.initTokenClient({
+      client_id: CLIENTID,
+      scope: SCOPES,
+      callback: (tokenResponse) => {
+        console.log(tokenResponse);
+        if (tokenResponse && tokenResponse.access_token) {
+          fetch('https://www.googleapis.com/drive/v3/files', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${tokenResponse.access_token}`,
+            },
+            body: JSON.stringify({ name: 'Test Sheet For Demo Day', mimeType: 'application/vnd.google-apps.spreadsheet' }),
+          });
+        }
+        // setTokenClient(tokenResponse);
+      },
+    }));
+
+    google.accounts.id.prompt();
+  }, []);
+  // const createSpreadsheet = async (fileName) => {
+  //   // const accessToken = ''; // insert access token method here;
+  //   console.log('reached create spreadsheet');
+  //   console.log(usertoken);
+  //   fetch(`https://docs.googleapis.com/v1/documents?title=${fileName}`, {
+  //     method: 'POST',
+  //     headers: new Headers({ Authorization: `Bearer ${usertoken}` }),
+  //   }).then((res) => res.json()).then((val) => {
+  //     console.log(val);
+  //     console.log(val.documentId);
+  //     window.open(`https://docs.google.com/document/d/${val.documentId}/edit`, '_blank');
+  //   });
+  // };
+
+  // const zerofill = (i) => (i < 10 ? '0' : '') + i;
+
+  const createDriveFile = () => {
+    tokenClient.requestAccessToken();
   };
 
   const handleClose = async (index) => {
@@ -45,6 +115,20 @@ function AvatarCard({ user, archivedUsers, setArchivedUsers }) {
       await deleteDocument(user.id);
       const newUsers = archivedUsers.filter((seeker) => seeker.id !== user.id);
       setArchivedUsers(newUsers);
+    }
+
+    if (index === 2) {
+      createDriveFile();
+      // google.accounts.id.renderButton(document.getElementById("signInDiv"), {theme: "outline", size: "large"});
+      // await signInWithGoogleAndCreateDoc(); // whatever is necessary to get me access token.
+      // // const date = new Date();
+      // // const year = date.getFullyear();
+      // // const month = zerofill(date.getMoneth() + 1);
+      // // const day = zerofill(date.getDate());
+      // // const currentDateString = `${year}-${month}-${day}`;
+      // // const currentTimeString = date.toLocaleTimeString();
+      // // createSpreadsheet(token, user.id + currentDateString + currentTimeString);
+      // createSpreadsheet('Test Doc 1');
     }
     setAnchorEl(null);
   };
