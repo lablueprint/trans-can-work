6f8d8d7f2a3fccc389befc0cdeb37bff1d322cd2
Aaron Shi <aaronshi@Aarons-MacBook-Pro.local>
2023-05-08 23:54:51 -0700

    download csv using test data in json

--- src/Components/AvatarCard.js
+++ src/Components/AvatarCard.js
@@ -10,12 +10,14 @@ import {
   doc, setDoc, deleteDoc,
 } from 'firebase/firestore';
 import { PropTypes } from 'prop-types';
+import userData from './SampleUserData.json';
 import { db } from './firebase';
 import './AvatarCard.css';
 
 const options = [
   'Approve',
   'Unarchive',
+  'Download',
   'Delete',
 ];
 
@@ -39,6 +41,77 @@ export default function AvatarCard({ user, archivedUsers, setArchivedUsers }) {
     await deleteDoc(doc(db, 'jobseekers', id));
   };
 
+  // for Download:
+  const downloadFile = ({ data, fileName, fileType }) => {
+    const blob = new Blob([data], { type: fileType });
+
+    const a = document.createElement('a');
+    a.download = fileName;
+    a.href = window.URL.createObjectURL(blob);
+    const clickEvt = new MouseEvent('click', {
+      view: window,
+      bubbles: true,
+      cancelable: true,
+    });
+    a.dispatchEvent(clickEvt);
+    a.remove();
+  };
+
+  const exportToCsv = () => {
+    console.log(userData);
+    const csv = ['User Information'];
+    csv.push([`authenticName,${userData.authenticName}`]);
+    csv.push([`pronouns,${userData.pronouns}`]);
+    csv.push([`bio,${userData.bio}`]);
+    csv.push([`role,${userData.role}`]);
+    csv.push([`approved,${userData.approved}`]);
+    csv.push([`uid,${userData.uid}`]);
+    csv.push([`navigator,${userData.navigator}`]);
+    csv.push([]);
+
+    csv.push(['ClientInfo']);
+
+    for (const key in userData.clientInfo) {
+      const value = userData.clientInfo[key];
+      csv.push([`${key},${value}`]);
+    }
+    csv.push([]);
+
+    csv.push(['Skills Checklists:']);
+    const skills = [];
+    for (const key in userData.skillsChecklist) {
+      const value = userData.skillsChecklist[key];
+      if (value) {
+        skills.push([`${key}`]);
+      }
+    }
+    csv.push(skills.join(','));
+    csv.push([]);
+
+    csv.push(['Industry Interests:']);
+    const ii = [];
+    for (const key in userData.industryInterest) {
+      const value = userData.industryInterest[key];
+      if (value) {
+        ii.push([`${key}`]);
+      }
+    }
+    csv.push(ii.join(','));
+
+    csv.push([]);
+    csv.push(['Milestone Progress:']);
+    for (const key in userData.milestones) {
+      const value = userData.milestones[key];
+      csv.push([`${key}, ${value}`]);
+    }
+
+    downloadFile({
+      data: csv.join('\n'),
+      fileName: 'aaron.csv',
+      fileType: 'text/csv',
+    });
+  };
+
   const handleClose = async (index) => {
     if (index === 0) {
       await approveUser(user.id);
@@ -51,6 +124,10 @@ export default function AvatarCard({ user, archivedUsers, setArchivedUsers }) {
       await deleteDocument(user.id);
       const newUsers = archivedUsers.filter((seeker) => seeker.id !== user.id);
       setArchivedUsers(newUsers);
+    }
+
+    if (index === 2) {
+      exportToCsv();
     }
     setAnchorEl(null);
   };
