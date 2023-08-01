593e2de2f372d6dc216f32744a1b4dbe45872658
angelahao6 <115599906+angelahao6@users.noreply.github.com>
2023-07-31 22:00:44 -0700

    Added external links

--- src/Components/Milestones/Resources.js
+++ src/Components/Milestones/Resources.js
@@ -6,12 +6,14 @@ function Resources() {
   return (
     <div>
       <h6 className="resource">Enter here fer access to a treasure trove o&apos; Resources.</h6>
-      <Button
-        disableRipple
-        id="resourceButton"
-      >
-        Resources Link
-      </Button>
+      <a href="https://example.com">
+        <Button
+          disableRipple
+          id="resourceButton"
+        >
+          Resources Link
+        </Button>
+      </a>
     </div>
   );
 }
