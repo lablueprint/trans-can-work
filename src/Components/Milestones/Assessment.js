f68f202a6a56961af8530ddfa0eb5fa08c08f1ae
angelahao6 <115599906+angelahao6@users.noreply.github.com>
2023-09-20 23:07:47 -0700

    Fix assessment css

--- src/Components/Milestones/Assessment.js
+++ src/Components/Milestones/Assessment.js
@@ -44,6 +44,11 @@ function Assessment({ jobseeker }) {
     start();
   }, []);
 
+  const dataSets = [
+    { data: jobseeker.degrees, label: 'Degree' },
+    { data: jobseeker.certificates, label: 'Certificate' },
+  ];
+
   return (
     <div className="assessment">
       <h6 className="contentTitle">Client Info</h6>
@@ -51,18 +56,19 @@ function Assessment({ jobseeker }) {
       <MilestoneClientInfo data={jobseeker.clientInfo} />
       <h6 className="contentTitle">Education Info</h6>
       <hr className="shortLine" />
-      <MilestoneClientInfo data={mapArrays(jobseeker.degrees, 'Degree')} />
-      <MilestoneClientInfo data={mapArrays(jobseeker.certificates, 'Certificate')} />
+      { dataSets.map((dataSet) => (
+        <MilestoneClientInfo data={mapArrays(dataSet.data, dataSet.label)} />
+      ))}
       <h6 className="contentTitle">Previous Experience</h6>
-      <p className="contentDescription">Yar, in what areas of these here industries do ye have actual work or volunteer experience?</p>
+      <p className="contentDescription">Yar, here are the industries ye have actual work or volunteer experience.</p>
       <hr className="longLine" />
       <MilestoneChecklist checkboxes={experiences} columns={2} />
       <h6 className="contentTitle">Industry Interests</h6>
-      <p className="contentDescription">In what of the followin&apos; industries are ye open to explorin&apos; or have an interest in possible future employment?</p>
+      <p className="contentDescription">Here are the industries ye is open to explorin&apos; or have an interest in possible future employment.</p>
       <hr className="shortLine" />
       <MilestoneChecklist checkboxes={industries} columns={2} />
       <h6 className="contentTitle">Skills Checklist</h6>
-      <p className="contentDescription">Please check all the skill sets that apply to ye.</p>
+      <p className="contentDescription">Here are the skill sets that apply to ye.</p>
       <hr className="shortLine" />
       <MilestoneChecklist checkboxes={skills} columns={2} />
       <h6 className="contentTitle">Ultimate Dream Job</h6>
